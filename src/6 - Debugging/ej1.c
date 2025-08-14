b78626a0e764a536a5c7268fff75fe6402740e51
brunocompu <brunoventrici@gmail.com>
2025-08-14 19:08:58 -0300

    Archivos para ejercicio de debugging
    Creo repo y subo archivos para resolver ejercicios de debugging

---
+++ src/6 - Debugging/ej1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ej1.h"
+
+// Marca el ejercicio 1 como hecho (`true`) o pendiente (`false`)
+bool EJERCICIO_1_HECHO = true;
+bool EJERCICIO_2_HECHO = true;
+bool EJERCICIO_3_HECHO = true;
+bool EJERCICIO_4_HECHO = true;
+
+uint64_t ejercicio1(uint64_t sum1, uint64_t sum2, uint64_t sum3, uint64_t sum4, uint64_t sum5){
+	return 0;
+}
+
+void ejercicio2(item_t* un_item, uint32_t id, uint32_t cantidad, char nombre[]){
+}
+
+uint32_t ejercicio3(uint32_t* array, uint32_t size, uint32_t (*fun_ej_3)(uint32_t a, uint32_t b)){
+	return 0;
+}
+
+uint32_t* ejercicio4(uint32_t** array, uint32_t size, uint32_t constante){
+	return NULL;
+}
---
+++ src/6 - Debugging/ej1.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+	char nombre[9];
+	uint32_t id;
+	uint32_t cantidad;
+} item_t;
+
+// Marca el ejercicio 1A como hecho (`true`) o pendiente (`false`).
+extern bool EJERCICIO_1_HECHO;
+extern bool EJERCICIO_2_HECHO;
+extern bool EJERCICIO_3_HECHO;
+extern bool EJERCICIO_4_HECHO;
+
+// OPCIONAL: implementar en C
+ 
+uint64_t ejercicio1(uint64_t sum1, uint64_t sum2, uint64_t sum3, uint64_t sum4, uint64_t sum5);
+
+void ejercicio2(item_t* un_item, uint32_t id, uint32_t cantidad, char nombre[]);
+
+uint32_t ejercicio3(uint32_t* array, uint32_t size, uint32_t (*fun_ej_3)(uint32_t a, uint32_t b));
+
+uint32_t* ejercicio4(uint32_t** array, uint32_t size, uint32_t constante);
