0c68ed52013b22cb75abd1099f7bbb8839cad7e7
mirko <mirkowolf46@gmail.com>
2025-09-09 14:52:33 -0300

    ej2 asm

--- src/2 - ABI/ABI.asm
+++ src/2 - ABI/ABI.asm
@@ -97,20 +97,74 @@ alternate_sum_4_using_c_alternative:
 
 
 ; uint32_t alternate_sum_8(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8);
-; registros y pila: x1[?], x2[?], x3[?], x4[?], x5[?], x6[?], x7[?], x8[?]
+; registros y pila: x1[EDI], x2[ESI], x3[EDX], x4[ECX], x5[R8], x6[R9], x7[RBP + 16], x8[RBP + 24]
 alternate_sum_8:
-	;prologo
+  push RBP ;pila alineada
+  mov RBP, RSP ;strack frame armado
+  sub RSP, 32 ; muevo el tope de la pila 8 bytes para guardar x4, y 8 bytes para que quede alineada
+  ;fin del prologo
 
-	; COMPLETAR
 
-	;epilogo
+  ; Guardo los valores que estaban en registros no volatiles en
+  mov [RBP-8], R9 ; x6
+  mov [RBP-16], R8 ; x5
+  mov [RBP-24], RCX ; x4
+  mov [RBP-32], RDX ; x3
+
+  call restar_c ; resta x2 a x1
+
+  mov EDI, EAX
+  mov ESI, [RBP - 32]
+  
+  call sumar_c ; sumo res + x3
+	
+  mov EDI, EAX
+  mov ESI, [RBP - 24]
+
+  call restar_c ; resta x4 a res
+
+  mov EDI, EAX
+  mov ESI, [RBP - 16]
+
+  call sumar_c ; sumo res + x5
+
+  mov EDI, EAX
+  mov ESI, [RBP - 8]
+
+  call restar_c ; resta x6 a res
+
+  mov EDI, EAX
+  mov ESI, [RBP + 16] ; por que 24 no
+
+  call sumar_c ; sumo res + x7
+
+  mov EDI, EAX
+  mov ESI, [RBP + 24]
+
+  call restar_c ; resta x8 a res
+
+
+  add RSP, 32 ;restauro tope de pila
+  pop RBP ;pila desalineada, RBP restaurado, RSP apuntando a la dirección de retorno
 	ret
 
 
 ; SUGERENCIA: investigar uso de instrucciones para convertir enteros a floats y viceversa
 ;void product_2_f(uint32_t * destination, uint32_t x1, float f1);
-;registros: destination[?], x1[?], f1[?]
+;registros: destination[RDI], x1[ESI], f1[XMM0]
 product_2_f:
+  push RBP ;pila alineada
+  mov RBP, RSP ;strack frame armado
+
+  cvtsi2ss xmm1, ESI        ; convierte a → float en xmm1
+
+  mulss xmm0, xmm1          ; xmm0 = xmm0 * xmm1  (b * a)
+
+  cvttss2si eax, xmm0       ; convertir a entero con truncamiento
+
+  mov [rdi], eax            ; *destination = resultado
+
+  pop RBP ;pila desalineada, RBP restaurado, RSP apuntando a la dirección de retorno
 	ret
 
 
@@ -118,24 +172,62 @@ product_2_f:
 ;, uint32_t x1, float f1, uint32_t x2, float f2, uint32_t x3, float f3, uint32_t x4, float f4
 ;, uint32_t x5, float f5, uint32_t x6, float f6, uint32_t x7, float f7, uint32_t x8, float f8
 ;, uint32_t x9, float f9);
-;registros y pila: destination[rdi], x1[?], f1[?], x2[?], f2[?], x3[?], f3[?], x4[?], f4[?]
-;	, x5[?], f5[?], x6[?], f6[?], x7[?], f7[?], x8[?], f8[?],
-;	, x9[?], f9[?]
+;registros y pila: destination[rdi], x1[ESI], f1[XMM0], x2[EDX], f2[XMM1], x3[ECX], f3[XMM2], x4[R8], f4[XMM3]
+;	, x5[R9], f5[XMM4], x6[BSP + 16], f6[XMM5], x7[BSP + 24], f7[XMM6], x8[BSP + 32], f8[XMM7],
+;	, x9[BSP + 40], f9[BSP + 48]
 product_9_f:
 	;prologo
-	push rbp
-	mov rbp, rsp
+  push RBP ;pila alineada
+  mov RBP, RSP ;strack frame armado
+  ;fin del prologo
 
 	;convertimos los flotantes de cada registro xmm en doubles
-	; COMPLETAR
+  cvtss2sd xmm0, xmm0
+  cvtss2sd xmm1, xmm1
+  cvtss2sd xmm2, xmm2
+  cvtss2sd xmm3, xmm3
+  cvtss2sd xmm4, xmm4
+  cvtss2sd xmm5, xmm5
+  cvtss2sd xmm6, xmm6
+  cvtss2sd xmm7, xmm7
+  cvtss2sd xmm8, [RBP + 48]
 
 	;multiplicamos los doubles en xmm0 <- xmm0 * xmm1, xmmo * xmm2 , ...
-	; COMPLETAR
+	mulsd xmm0, xmm1
+  mulsd xmm0, xmm2
+  mulsd xmm0, xmm3
+  mulsd xmm0, xmm4
+  mulsd xmm0, xmm5 
+  mulsd xmm0, xmm6
+  mulsd xmm0, xmm7 
+  mulsd xmm0, xmm8
 
 	; convertimos los enteros en doubles y los multiplicamos por xmm0.
-	; COMPLETAR
+	cvtsi2sd xmm1, esi
+  cvtsi2sd xmm2, edx 
+  cvtsi2sd xmm3, ecx 
+  cvtsi2sd xmm4, r8 
+  cvtsi2sd xmm5, r9
+  cvtsi2sd xmm6, [RBP + 16]
+  cvtsi2sd xmm7, [RBP + 24]
+  cvtsi2sd xmm8, [RBP + 32]
+  cvtsi2sd xmm9, [RBP + 40]
+
+	
+	mulsd xmm0, xmm1
+  mulsd xmm0, xmm2
+  mulsd xmm0, xmm3
+  mulsd xmm0, xmm4
+  mulsd xmm0, xmm5 
+  mulsd xmm0, xmm6
+  mulsd xmm0, xmm7 
+  mulsd xmm0, xmm8
+  mulsd xmm0, xmm9
+
+
+  movsd [rdi], xmm0            ; *destination = resultado
 
 	; epilogo
-	pop rbp
+  pop RBP ;pila desalineada, RBP restaurado, RSP apuntando a la dirección de retorno
 	ret
 
--- src/2 - ABI/main.c
+++ src/2 - ABI/main.c
@@ -7,10 +7,21 @@
 #include "../test-utils.h"
 #include "ABI.h"
 
-int main() {
+int main()
+{
 	/* Acá pueden realizar sus propias pruebas */
-	assert(alternate_sum_4_using_c(8, 2, 5, 1) == 6);
+	 assert(alternate_sum_4_using_c(8, 2, 5, 1) == 10);
 
-	assert(alternate_sum_4_using_c_alternative(8, 2, 5, 1) == 6);
+	assert(alternate_sum_4_using_c_alternative(8, 2, 5, 1) == 10);
+	
+	assert((alternate_sum_8(8, 2, 4, 5, 20, 15, 8, 16)) == 2);
+
+	uint32_t prod2;
+	product_2_f(&prod2, 3, 13.1);
+	assert(prod2 == 39);
+
+	double prod9;
+	product_9_f(&prod9, 2, 1.5, 2, 1.5, 2, 1.5, 2, 1.5, 2, 1.5, 2, 1.5, 2, 1.5, 2, 1.5, 2, 1.5);
+	assert(prod9 == 19683);
 	return 0;
 }
