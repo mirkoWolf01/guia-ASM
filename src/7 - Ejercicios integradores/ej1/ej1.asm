1e5a069f9148f8a27e15946e08629d301bcb47ca
mirko <mirkowolf46@gmail.com>
2025-09-14 21:31:23 -0300

    ej 7.1 hecho

--- src/7 - Ejercicios integradores/ej1/ej1.asm
+++ src/7 - Ejercicios integradores/ej1/ej1.asm
@@ -14,21 +14,21 @@ TRUE  EQU 1
 ; Funciones a implementar:
 ;   - es_indice_ordenado
 global EJERCICIO_1A_HECHO
-EJERCICIO_1A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - indice_a_inventario
 global EJERCICIO_1B_HECHO
-EJERCICIO_1B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_1B_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-ITEM_NOMBRE EQU ??
-ITEM_FUERZA EQU ??
-ITEM_DURABILIDAD EQU ??
-ITEM_SIZE EQU ??
+ITEM_NOMBRE EQU 0
+ITEM_FUERZA EQU 20
+ITEM_DURABILIDAD EQU 24
+ITEM_SIZE EQU 28
 
 ;; La funcion debe verificar si una vista del inventario está correctamente 
 ;; ordenada de acuerdo a un criterio (comparador)
@@ -53,8 +53,7 @@ ITEM_SIZE EQU ??
 ;; - Importa que los ítems estén ordenados según el comparador. No hay necesidad
 ;;   de verificar que el orden sea estable.
 
-global es_indice_ordenado
-es_indice_ordenado:
+
 	; Te recomendamos llenar una tablita acá con cada parámetro y su
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
@@ -63,7 +62,68 @@ es_indice_ordenado:
 	; r/m64 = uint16_t*    indice
 	; r/m16 = uint16_t     tamanio
 	; r/m64 = comparador_t comparador
-		ret
+
+global es_indice_ordenado
+es_indice_ordenado:
+	push rbp
+	mov rbp, rsp
+	push R12
+	push R13
+	push R14
+	push R15 
+	sub rsp, 16; alineado a 16b
+	;prologo
+	
+
+	mov R12, RDI ; inventario
+	mov R13, RSI ; indice
+	movzx R14, DX ; tamaño
+	mov R15, RCX ; comparador
+
+	XOR RDI, RDI
+	mov DI, WORD [R13] ; guardo en RDI -> indice[0]
+	imul RDI, 8 ; multiplico el valor del indice por el tamaño de un puntero (8 bytes)
+	mov RDI, [R12 + RDI] ; calculo el puntero al elemento indice[0]
+
+	mov RCX, 1
+	mov [rbp - 40], RCX ; indice dentro del ciclo
+	add R13, 2
+	.ciclo: 
+		cmp [rbp - 40], R14W
+		mov rax, TRUE
+		jnl .epilogo
+		
+		xor RDX, RDX
+		mov DX, WORD [R13]
+		imul RDX, 8
+		mov RSI, [R12 + RDX] ; accedo al elemento [0] del indice 
+
+		mov [rbp - 48], RSI ; guardo el elemento anterior
+
+		call R15 ; llamo al comparador
+		cmp rax, 0
+		je .caso_desordenado
+		
+		mov RDI, [rbp - 48] ; actualizo el item anterior
+
+		mov RCX, [rbp - 40] ; i++
+		inc RCX ; aumento el iterador local
+		mov [rbp - 40], RCX
+
+		add R13, 2 ; voy a la siguiente posicion del indice
+		jmp .ciclo
+
+	.caso_desordenado:
+	mov rax, FALSE
+
+	.epilogo:
+	add rsp, 16
+	pop R15
+	pop R14 
+	pop R13
+	pop R12 
+	pop rbp 
+	ret
 
 ;; Dado un inventario y una vista, crear un nuevo inventario que mantenga el
 ;; orden descrito por la misma.
@@ -85,8 +145,8 @@ es_indice_ordenado:
 ;;   `ítems`. Se pide *copiar* estos punteros, **no se deben crear ni clonar
 ;;   ítems**
 
-global indice_a_inventario
-indice_a_inventario:
+
+
 	; Te recomendamos llenar una tablita acá con cada parámetro y su
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
@@ -94,4 +154,50 @@ indice_a_inventario:
 	; r/m64 = item_t**  inventario
 	; r/m64 = uint16_t* indice
 	; r/m16 = uint16_t  tamanio
+
+global indice_a_inventario
+indice_a_inventario:
+	push rbp
+	mov rbp, rsp
+	push R12
+	push R13
+	push R14
+	sub RSP, 8 ; queda alineado a 16
+	; prologo
+
+	mov R12, RDI  ; inventario
+	mov R13, RSI  ; indice
+	movzx R14, DX ; tamaño
+
+
+	imul RDI, R14, 8
+
+	call malloc
+
+
+	mov RCX, 0
+	.ciclo: 
+		cmp RCX, R14
+		jnl .epilogo
+
+		XOR RDX, RDX
+		mov DX, WORD [R13] ; guardo en RDI -> indice[i]
+		imul RDX, 8 ; multiplico el valor del indice por el tamaño de un puntero (8 bytes)
+		mov RDI, [R12 + RDX] ; calculo el puntero al elemento indice[i]
+
+
+		imul RDX, RCX, 8 ; defino el indice donde escribo
+		mov [RAX + RDX], RDI ; guardo en la direccion [puntero + offset] el puntero al struct
+
+		add R13, 2 ; voy a la siguiente posicion del indice
+		inc RCX
+		jmp .ciclo
+
+
+	.epilogo:
+	add rsp, 8
+	pop R14
+	pop R13 
+	pop R12
+	pop rbp
 	ret
